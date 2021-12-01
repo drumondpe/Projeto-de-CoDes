65d4029a3f641e79b7bfc9aa0fb83823e53e7358
drumondpe <pedrogsd@al.insper.edu.br>
2021-12-01 15:03:14 -0300

    Teste

--- Projeto/template.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    botao = document.querySelector('#salvar')
-    botao.addEventListener('click', function(event){
-    botao.innerHTML = 'Salvo!'
-    botao.style.color = 'rgb(0, 255, 0)'
-    botao.style.borderColor = 'rgb(0, 255, 0)'
-
-    })
-})
